fb9f73b8859f45f627044d6e1ce743f8e3ee5f3d
Brahmadev Rai <raibramadev601@gmail.com>
2025-02-15 14:48:11 +0530

    add-chat-bot

--- Backend/controllers/chatbot.js
+++ Backend/controllers/chatbot.js
@@ -1,10 +1,10 @@
 const Products = require("../models/products");
 const PlaceOrders = require("../models/placeOrders");
-const { OpenAI } = require("openai");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+// const { OpenAI } = require("openai");
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+// const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 
 const chatbotController = async (req, res) => {
